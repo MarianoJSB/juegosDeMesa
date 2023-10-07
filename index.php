1885880baaf185bcfd37378856ecbaebe4a6900a
maxxi1234 <aulicino.maximo@tecnica7.edu.ar>
2023-10-07 11:27:34 -0300

    Actualizo el header,agredo carpeta imagenes con el logo(logo listo)

--- index.php
+++ index.php
@@ -14,16 +14,17 @@
 </head>
 <body>
     <header>
-        <div class="left">           
-            <div class="title">
-                <a href="http://localhost/juegosDeMesa/juegosDeMesa/index.php" style="text-decoration: none; color: black;"><h1>Logo</h1></a>
-            </div>
-        </div>
+    <div class="left">
+        <a href="http://localhost/juegosDeMesa/juegosDeMesa/index.php" style="text-decoration: none; color: black; display: flex; align-items: center;">
+            <img src="imagenes/logo.png" alt="Logo" style="height: 55px; margin-left: 55px;">
+            <h1>Game Board Store</h1>
+        </a>
+    </div>
         <div class="right">
             <div class="buscador">                            
                 <div class="bot">     
                     <input type="checkbox" id="toggle-theme" checked>
-                    <label id="darkMenu" for="toggle-theme"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!-- Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc.--><path d="M448 256c0-106-86-192-192-192V448c106 0 192-86 192-192zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256z"/></svg></label>
+                    <label id="darkMenu" for="toggle-theme"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M448 256c0-106-86-192-192-192V448c106 0 192-86 192-192zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256z"/></svg></label>
                     
 
                     <label for="busquedaJuego"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#ffffff}</style><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg></label>
